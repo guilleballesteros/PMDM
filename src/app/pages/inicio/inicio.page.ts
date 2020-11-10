3acd75478ee589e0d1601da92da11f8413aac6a2
guilleballesteros <bugfiher@outlook.com>
2020-11-10 18:50:14 +0100

    login

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+
+  constructor(private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
+  }
+
+  OnSubmitLogin() {
+    this.authService.login(this.email, this.password).then(res => {
+    this.router.navigate(['/gestion']);
+    }).catch(err => alert('Los datos son incorrectos o no existen'));
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,16 @@ export const environment = {
   production: false
 };
 
+export const firebaseConfig = {
+  apiKey: 'AIzaSyB17YyiHTm1lpKlpcuCZYkJKD7lTKB3fqA',
+  authDomain: 'pmdm-2c.firebaseapp.com',
+  databaseURL: 'https://pmdm-2c.firebaseio.com',
+  projectId: 'pmdm-2c',
+  storageBucket: 'pmdm-2c.appspot.com',
+  messagingSenderId: '70424745193',
+  appId: '1:70424745193:web:65f24f2128dd43f6984c78'
+};
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
