453ce8f7122fdb99320cbda0c00d95d13465f84a
pablodelgadom <pablo00dm00@gmail.com>
2020-11-10 18:18:40 +0100

    gfch

--- src/app/pages/gestion/gestion.page.ts
+++ src/app/pages/gestion/gestion.page.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GestionPage implements OnInit {
 
+  componentes: Componente[] = [
+    {
+    icon : 'american-football',
+    name: 'listado de fichas',
+    redirectTo: 'listado'
+    },
+    {
+    icon : 'football-outline',
+    name: 'Alert',
+    redirectTo: 'creacion de fichas'
+    },
+    {
+      icon : 'beaker',
+      name: 'Avatar',
+      redirectTo: 'asistencia'
+    },
+    ];
+
   constructor() { }
 
   ngOnInit() {
