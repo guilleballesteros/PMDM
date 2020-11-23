5337d22de3b7490518152186e4fd3ecf2d6f4652
YerayDP <yerayd99@gmail.com>
2020-11-23 12:25:51 +0100

    Yeray

--- src/app/pages/asistencia/asistencia.page.ts
+++ src/app/pages/asistencia/asistencia.page.ts
@@ -11,61 +11,67 @@ import { runInThisContext } from 'vm';
 })
 export class AsistenciaPage implements OnInit {
 
+
   eventSource = [];
+
   calendar = {
     mode: 'month',
     currentDate: new Date(),
-
   };
   selectedDate = new Date();
 
-  constructor(private db : AngularFirestore,) { 
-
-    this.db.collection('events').snapshotChanges().subscribe(colSnap => {
+  constructor(private db: AngularFirestore,) {
+    this.db.collection(`events`).snapshotChanges().subscribe(colSnap => {
       this.eventSource = [];
-      colSnap.forEach(snap=>{
+      colSnap.forEach(snap => {
         let event:any = snap.payload.doc.data();
         event.id = snap.payload.doc.id;
-        event.startTime= event.starTime.toDate();
-        event.endTime= event.endTime.toDate();
+        event.startTime = event.startTime.toDate();
+        event.endTime = event.endTime.toDate();
+        console.log(event);
         this.eventSource.push(event);
       });
     });
   }
 
-  ngOnInit() {
+  ngOnInit(){
+    
   }
-
-  addNewEvent(){
-    let start= this.selectedDate;
+  addNewEvent() {
+    let start = this.selectedDate;
     let end = this.selectedDate;
-    end.setMinutes(end.getMinutes()+60);
-    let event={
-      title: 'Event # - ' + start.getMinutes(),
+    end.setMinutes(end.getMinutes() + 60);
+
+    let event = {
+      title: 'Event #' + start.getMinutes(),
       startTime: start,
-      endTime : end,
-      allDay:false
-    }
-    this.db.collection('events').add(event);
+      endTime: end,
+      allDay: false,
+    };
+
+    this.db.collection(`events`).add(event);
   }
 
-  onViewTitleChanged(title){
-    console.log(title)
+  onViewTitleChanged(title) {
+    console.log(title);
   }
-  onEventSelected(event){
-    console.log('Event selected: '+ event.starTime + '-'+ event.endTime + '-' + event.title)
+
+  onEventSelected(event) {
+    console.log('Event selected:' + event.startTime + '-' + event.endTime + ',' + event.title);
   }
-  onTimeSelected(ev){
-    console.log('Selected time: ' +ev.selectedTime + ', hasEvents: '+
-    (ev.events !== undefined && ev.events.length !== 0) +', disabled: '+ ev.disabled)
-    this.selectedDate=ev.selectedTime;
-    console.log(this.selectedDate)
+
+  onTimeSelected(ev) {
+    console.log('Selected time: ' + ev.selectedTime + ', hasEvents: ' +
+      (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
+    this.selectedDate = ev.selectedTime;
   }
-  onCurrentDateChanged(event: Date){
-    console.log('Current date change: ' + event)
+
+  onCurrentDateChanged(event: Date) {
+    console.log('current date change: ' + event);
   }
-  onRangeChanged(ev){
-    console.log('Range changed: starTime: ' + ev.starTime + ', endTime: ' + ev.endTime)
+
+  onRangeChanged(ev) {
+    console.log('range changed: startTime: ' + ev.startTime + ', endTime: ' + ev.endTime);
   }
 
 }
